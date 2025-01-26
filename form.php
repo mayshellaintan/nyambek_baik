9656a2881a77b3f3d96ec5938f7a4d21249121a2
mayshellaintan <mayshelaintan@gmail.com>
2025-01-26 12:53:39 +0700

    add:text

---
+++ form.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viwport"content="width=device-width, initial-
+scale=1.0">
+</head>
+<body>
+<fieldset>
+     <form action=""method="POST">
+     
+<p>
+
+     <label>NISN :</label>
+    <input type="text" name="NISN">
+
+</p>
+<p>
+     <label>nama lengkap :</label>
+     <input type="text" name="nama lengkap" placeholder="Masukan
+nama lengkap">
+</p>
+<p>
+     <label>tempat lahir :</label>
+     <input type="text" name="tempat lahir" placeholder="Masukan tempat lahir">
+</p>
+<p>
+     <label>tanggal lahir :</label>
+     <input type="date" name="tanggal lahir" placeholder="masukan tanggal lahir">
+</p>
+<p>
+     <label>Jenis Kelamin :</label>
+     <input type="radio" name="jenkel" value="laki-laki">Laki-laki
+     <input type="radio" name="jenkel" value="perempuan">Perempuan
+</p>
+<p>
+     <label>Agama :</label>
+     <select name="agama">
+         <option value="budha">Budha</option>
+         <option value="hindu">Hindu</option>
+         <option value="islam">Islam</option>
+         <option value="katholik">Kristen Katholik</option>
+         <option value="protestan">Kristen Protestan</option>
+         <option value="konghucu">kristen konghucu</option>
+     </select>
+</p>
+<p>
+     <p>Alamat :</p>
+     <textarea name="alamat"cols="50" rows="10"></textarea>
+</p>
+<p>
+     <label>asal sekolah :</label>
+     <input type="text" name="asal sekolah">
+</p>
+<p>
+      <label>jurusan 1 :</label>
+      <select name value="JURUSAN">
+          <option value="PPLG">PENGEMBANGAN PERANGKAT LUNAK DAN GIM</option>
+          <option value="TJKT">TEKNIK JARINGAN KOMPUTER DAN TELEKOMUNIKASI</option>
+          <option value="DKV">DESAIN KOMUNIKASI VISUAL</option>
+          <option value="SP">SENI PERTUNJUKAN</option>
+          <option value="AKL">AKUNTANSI KEUANGAN LEMBAGA</option>
+          <option value="PH">PERHOTELAN</option>
+          <option value="PM">PEMASARAN</option>
+          <option value="BCF">BROADCASTING DAN PERFILMAN</option>
+     </select>     
+
+</p>
+<p>        
+      <label>jurusan 2  :</label>
+      <select name value="JURUSAN">
+          <option value="PPLG">PENGEMBANGAN PERANGKAT LUNAK DAN GIM</option>
+          <option value="TJKT">TEKNIK JARINGAN KOMPUTER DAN TELEKOMUNIKASI</option>
+          <option value="DKV">DESAIN KOMUNIKASI VISUAL</option>
+          <option value="SP">SENI PERTUNJUKAN</option>
+          <option value="AKL">AKUNTANSI KEUANGAN LEMBAGA</option>
+          <option value="PH">PERHOTELAN</option>
+          <option value="PM">PEMASARAN</option>
+          <option value="BCF">BROADCASTING DAN PERFILMAN</option>
+     </select>     
+
+</p>
+<p>
+
+    <label>Ektrakulikuler:</label>
+    
+    <input type="checkbox" name="OSIS">ORGANISASI SISWA INTRA SEKOLAH
+    <input type="checkbox" name="DA">DEWAN AMBALAN
+    <input type="checkbox" name="PA">PECINTA ALAM
+    <input type="checkbox" name="REMAS">REMAS AL-KAUTSAR
+    <input type="checkbox" name="UKS/PMR">USAHA KESEHATAN SEKOLAH
+    <input type="checkbox" name="FUTSAL">FUTSAL
+    <input type="checkbox" name="BASKET">BASKET
+    <input type="checkbox" name="VOLI">VOLI
+    <input type="checkbox" name="TEATER KUSUMA">TEATER KUSUMA
+    <input type="checkbox" name="LH">LASKAR HIJAU
+    <input type="checkbox" name="GDS">GERAKAN DISIPLIN SEKOLAH
+    
+</p>
+<p>
+     
+     <input type="submit" name="submit" value="Daftar">
+    </p>
+    </fieldset>
+</form>
+</body>
+</html> 
